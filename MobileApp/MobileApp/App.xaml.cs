3132d41d8ac0c7f6daa8f5d25e15b429a5a6f229
Rafael Oviedo Hidalgo <96155602+rovi09@users.noreply.github.com>
2022-02-15 15:57:42 -0600

    Initial commit

---
+++ MobileApp/MobileApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MobileApp.Views;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+	public partial class App
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
+		}
+
+		private Page UseTabs()
+		{
+			TabsPage tabs;
+			MainPage = tabs = new TabsPage();
+
+			tabs.Children.Add(new FontsView() { Title = "Font Families" });
+			tabs.Children.Add(new FontSample("Lobster") { Title = "Font sample" });
+
+			return tabs;
+		}
+	}
+}
---
+++ MobileApp/MobileApp/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: Preserve(AllMembers = true)]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+[assembly: ExportFont("ArchitectsDaughter-Regular.ttf", Alias = "ArchitectsDaughter")]
